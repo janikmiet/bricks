0b7b959b8b2abf1ea0a4ee92a7b6901732c83b06
janitest <jani.miettinen@kela.fi>
2017-07-10 14:41:20 +0300

    Vanha versio backupista

---
+++ R/makeOLDBCKP.R
@@ -0,0 +1,44 @@
+
+## mytMAKEBCKP --------------------------------------------------------------------------------------------
+# This script makes zipped backup copy of users folder without RDS, RDATA or CSV datafiles.
+# Updated: 29-03-2017
+
+makeOLDBCKP <- function(x){
+
+  # WD
+  curWD <- getwd()
+  setwd("~/")
+
+  ## List all files from root directory:
+  d <- "~/"
+  myFiles <- list.files(d, recursive=TRUE)
+
+
+  # Exclude datasets, zip-files and library!
+  if(TRUE){
+    myFiles <- myFiles[!grepl("*.csv", tolower(myFiles))] # ei csv
+    myFiles <- myFiles[!grepl("*.rds",tolower(myFiles))] # ei rds
+    myFiles <- myFiles[!grepl("*.rdata",tolower(myFiles))] # ei rdata
+    myFiles <- myFiles[!grepl("*.sas7bdat",tolower(myFiles))] # ei sasdata
+    myFiles <- myFiles[!grepl("shinylogs.txt",tolower(myFiles))] # ei shinylogia
+    myFiles <- myFiles[!grepl("*.zip",tolower(myFiles))] # ei zippejä
+    myFiles <- myFiles[!grepl("x86_64-redhat-linux-gnu-library/", tolower(myFiles))] # ei librarya
+  }
+
+  ## Zip all files to root:
+  fileName <- paste0("bckp-", Sys.info()[["user"]], "_", Sys.Date(), ".zip")
+  zip(fileName, files=myFiles)
+
+
+  # Back to wd
+  setwd(curWD)
+
+  ### Testing:
+  # dir.create("./temp/")
+  # unzip(fileName, exdir = "./temp")
+
+}
+
+# END
+
+
